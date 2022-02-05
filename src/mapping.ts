600627a383a81cf293d1d8a716912a4853346c76
Max Klenk <max@klenk.biz>
2022-02-05 23:02:34 +0100

    update schema to match naming

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,7 +1,6 @@
-import {
-  LiFiTransferStarted
-} from "../generated/LiFiDiamond/LiFiDiamond"
-import { LiFiTransfer } from "../generated/schema";
+import { dataSource } from '@graphprotocol/graph-ts'
+import { LiFiTransferStarted } from '../generated/LiFiDiamond/LiFiDiamond'
+import { LiFiTransfer } from '../generated/schema'
 
 /*
 * @param event - The contract event to update the subgraph record with
@@ -9,29 +8,29 @@ import { LiFiTransfer } from "../generated/schema";
 export function handleLiFiTransferStarted(event: LiFiTransferStarted): void {
 
   // load or create entity for transactionId
-  let transferId = event.params.transactionId.toHex();
-  let lifiTransfer = LiFiTransfer.load(transferId);
+  let transferId = event.params.transactionId.toHex()
+  let lifiTransfer = LiFiTransfer.load(transferId)
   if (lifiTransfer == null) {
-    lifiTransfer = new LiFiTransfer(transferId);
+    lifiTransfer = new LiFiTransfer(transferId)
   }
 
   // store event data in entity
-  lifiTransfer.integrator = event.params.integrator;
-  lifiTransfer.referrer = event.params.referrer;
-  lifiTransfer.sendingAssetId = event.params.sendingAssetId;
-  lifiTransfer.receivingAssetId = event.params.receivingAssetId;
-  lifiTransfer.receiver = event.params.receiver;
-  lifiTransfer.amount = event.params.amount;
-  lifiTransfer.destinationChainId = event.params.destinationChainId;
+  lifiTransfer.integrator = event.params.integrator
+  lifiTransfer.referrer = event.params.referrer
+  lifiTransfer.fromTokenAddress = event.params.sendingAssetId
+  lifiTransfer.toTokenAddress = event.params.receivingAssetId
+  lifiTransfer.toAddress = event.params.receiver
+  lifiTransfer.fromAmount = event.params.amount
+  lifiTransfer.toChainId = event.params.destinationChainId.toI32()
 
   // store additional information
   lifiTransfer.timestamp = event.params.timestamp
   lifiTransfer.gasLimit = event.transaction.gasLimit
   lifiTransfer.gasPrice = event.transaction.gasPrice
-  lifiTransfer.sender = event.transaction.from
+  lifiTransfer.fromAddress = event.transaction.from
   lifiTransfer.transactionHash = event.transaction.hash
 
-  // call
+  // parse bridge
   const callSignature = event.transaction.input.toHexString()
   if (callSignature.startsWith('0xe18a8fdb')) {
     lifiTransfer.bridge = 'multichain'
@@ -58,6 +57,68 @@ export function handleLiFiTransferStarted(event: LiFiTransferStarted): void {
     lifiTransfer.bridge = 'cbridge'
     lifiTransfer.hasSourceSwap = true
   }
+
+  // parse fromChainId
+  let network = dataSource.network()
+  let chainId = 0
+  if (network == 'mainnet') {
+    chainId = 1
+  } else if (network == 'ropsten') {
+    chainId = 3
+  } else if (network == 'rinkeby') {
+    chainId = 4
+  } else if (network == 'goerli') {
+    chainId = 5
+  } else if (network == 'optimism') {
+    chainId = 10
+  } else if (network == 'kovan') {
+    chainId = 42
+  } else if (network == 'bsc') {
+    chainId = 56
+  } else if (network == 'optimism-kovan') {
+    chainId = 69
+  } else if (network == 'poa-sokol') {
+    chainId = 77
+  } else if (network == 'chapel') {
+    chainId = 97
+  } else if (network == 'poa-core') {
+    chainId = 99
+  } else if (network == 'xdai') {
+    chainId = 100
+  } else if (network == 'fuse') {
+    chainId = 122
+  } else if (network == 'matic') {
+    chainId = 137
+  } else if (network == 'fantom') {
+    chainId = 250
+  } else if (network == 'clover') {
+    chainId = 1024
+  } else if (network == 'moonriver') {
+    chainId = 1285
+  } else if (network == 'mbase') {
+    chainId = 1287
+  } else if (network == 'arbitrum-one') {
+    chainId = 42161
+  } else if (network == 'celo') {
+    chainId = 42220
+  } else if (network == 'fuji') {
+    chainId = 43113
+  } else if (network == 'avalanche') {
+    chainId = 43114
+  } else if (network == 'celo-alfajores') {
+    chainId = 44787
+  } else if (network == 'mumbai') {
+    chainId = 80001
+  } else if (network == 'arbitrum-rinkeby') {
+    chainId = 421611
+  } else if (network == 'aurora') {
+    chainId = 1313161554
+  } else if (network == 'aurora-testnet') {
+    chainId = 1313161555
+  }
+  // near-mainnet
+  // near-testnet
+  lifiTransfer.fromChainId = chainId
 
   // save changes
   lifiTransfer.save()
